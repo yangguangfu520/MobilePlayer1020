d301f3bd14c8a7eaf029032af4910506b7df36e5
atguiguafu <541433511@qq.com>
2017-01-06 16:45:44 +0800

    基类BaseFragment

---
+++ app/src/main/java/com/atguigu/mobileplayer1020/base/BaseFragment.java
@@ -0,0 +1,71 @@
+package com.atguigu.mobileplayer1020.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 作者：尚硅谷-杨光福 on 2017/1/6 16:39
+ * 微信：yangguangfu520
+ * QQ号：541433511
+ * 作用：基类Fragment
+ */
+public abstract class BaseFragment extends Fragment {
+
+    public Context mContext;
+
+    /**
+     * 当系统创建当前BaseFragment类的时候回调
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    /**
+     * 当系统要创建Fragment的视图的时候回调这个方法
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    /**
+     * 抽象方法，让孩子实现
+     * @return
+     */
+    public abstract View initView() ;
+
+    /**
+     * 当Activty创建成功的时候回调该方法
+     * 初始化数据：
+     * 联网请求数据
+     * 绑定数据
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    /**
+     *当子类需要：
+     * 1.联网请求网络，的时候重写该方法
+     * 2.绑定数据
+     */
+    public void initData() {
+
+    }
+}
