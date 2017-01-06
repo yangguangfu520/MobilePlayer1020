f38fc151c65c870335bb499d7bd3178976423df9
atguiguafu <541433511@qq.com>
2017-01-06 14:53:39 +0800

    进入MainActivity主页面和Activity启动模式

--- app/src/main/java/com/atguigu/mobileplayer1020/WelcomeActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/WelcomeActivity.java
@@ -1,13 +1,56 @@
 package com.atguigu.mobileplayer1020;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //延迟两秒进入主页面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+//    private boolean isStartMain = false;
+
+    /**
+     * 进入主页面
+     */
+    private void startMainActivity() {
+//        if(!isStartMain){
+//            isStartMain = true;
+            //1.进入主页面
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            //2.关闭当前页面
+            finish();
+//        }
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //移除所有消息
+        handler.removeCallbacksAndMessages(null);
     }
 }
