49d00fc0e6d84f9970810bb0854c445934697da9
atguiguafu <541433511@qq.com>
2017-01-06 16:33:42 +0800

    底部RadioGroup

--- app/src/main/java/com/atguigu/mobileplayer1020/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/MainActivity.java
@@ -2,12 +2,18 @@ package com.atguigu.mobileplayer1020;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup rg_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg_main = (RadioGroup) findViewById(R.id.rg_main);
+
+        //默认选中本地视频
+        rg_main.check(R.id.rb_local_video);
     }
 }
