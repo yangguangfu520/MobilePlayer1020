6d58ae3169c02744b8b8d781012a0eac96d67d61
atguiguafu <541433511@qq.com>
2017-01-07 14:15:07 +0800

    解决安卓6.0以上版本不能读取外部存储权限的问题

--- app/src/main/java/com/atguigu/mobileplayer1020/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/MainActivity.java
@@ -1,5 +1,9 @@
 package com.atguigu.mobileplayer1020;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         rg_main = (RadioGroup) findViewById(R.id.rg_main);
 
+        isGrantExternalRW(this);
 
         initFragment();
 
@@ -136,4 +141,29 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.e("TAG","onDestroy");
     }
+
+
+    /**
+     * 解决安卓6.0以上版本不能读取外部存储权限的问题
+     * @param activity
+     * @return
+     */
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+
 }
