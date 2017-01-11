8b381a4fa61f09ddf4ca0bf8a24ca0bdc77ddfe0
atguiguafu <541433511@qq.com>
2017-01-11 15:13:43 +0800

    音乐播放器页面的完成

---
+++ app/src/main/java/com/atguigu/mobileplayer1020/activity/SystemAudioPlayerActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.mobileplayer1020.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atguigu.mobileplayer1020.R;
+
+public class SystemAudioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_audio_player);
+    }
+}
--- app/src/main/java/com/atguigu/mobileplayer1020/fragment/LocalAudioFragment.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/fragment/LocalAudioFragment.java
@@ -4,7 +4,6 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
@@ -15,7 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.atguigu.mobileplayer1020.R;
-import com.atguigu.mobileplayer1020.activity.SystemVideoPlayerActivity;
+import com.atguigu.mobileplayer1020.activity.SystemAudioPlayerActivity;
 import com.atguigu.mobileplayer1020.adapter.LocalVideoAdapter;
 import com.atguigu.mobileplayer1020.base.BaseFragment;
 import com.atguigu.mobileplayer1020.bean.MediaItem;
@@ -76,28 +75,9 @@ public class LocalAudioFragment extends BaseFragment {
 
         @Override
         public void onItemClick(AdapterView<?> parent,View view, int position, long id) {
-            MediaItem mediaItem = mediaItems.get(position);
-//            Toast.makeText(mContext, "mediaItem=="+mediaItem.toString(), Toast.LENGTH_SHORT).show();
-            //
-            //1.调起系统的播放器播放视频--隐式意图
-//            Intent intent = new Intent();
-//            //第一参数：播放路径
-//            //第二参数：路径对应的类型
-//            intent.setDataAndType(Uri.parse(mediaItem.getData()),"video/*");
-//            startActivity(intent);
-            //2.调起自定义播放器
-//            Intent intent = new Intent(mContext,SystemVideoPlayerActivity.class);
-//            //第一参数：播放路径
-//            //第二参数：路径对应的类型
-//            intent.setDataAndType(Uri.parse(mediaItem.getData()),"video/*");//一个播放地址
-//            startActivity(intent);
             //3.传递列表数据
-            Intent intent = new Intent(mContext,SystemVideoPlayerActivity.class);
+            Intent intent = new Intent(mContext,SystemAudioPlayerActivity.class);
 
-            Bundle bundle = new Bundle();
-            //列表数据
-            bundle.putSerializable("videolist",mediaItems);
-            intent.putExtras(bundle);
             //传递点击的位置
             intent.putExtra("position",position);
             startActivity(intent);
