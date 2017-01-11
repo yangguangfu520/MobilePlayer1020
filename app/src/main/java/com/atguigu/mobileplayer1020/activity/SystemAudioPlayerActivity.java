84af07d052743e7b34559c2d99014b24c7015d1f
atguiguafu <541433511@qq.com>
2017-01-11 15:23:22 +0800

    动画列表-帧动画

--- app/src/main/java/com/atguigu/mobileplayer1020/activity/SystemAudioPlayerActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/activity/SystemAudioPlayerActivity.java
@@ -1,15 +1,85 @@
 package com.atguigu.mobileplayer1020.activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.atguigu.mobileplayer1020.R;
 
-public class SystemAudioPlayerActivity extends AppCompatActivity {
+public class SystemAudioPlayerActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView ivIcon;
+    private TextView tvArtist;
+    private TextView tvName;
+    private TextView tvTime;
+    private SeekBar seekbarAudio;
+    private Button btnAudioPlaymode;
+    private Button btnAudioPre;
+    private Button btnAudioStartPause;
+    private Button btnAudioNext;
+    private Button btnSwichLyric;
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2017-01-11 15:19:08 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        setContentView(R.layout.activity_system_audio_player);
+        tvArtist = (TextView) findViewById(R.id.tv_artist);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        seekbarAudio = (SeekBar) findViewById(R.id.seekbar_audio);
+        btnAudioPlaymode = (Button) findViewById(R.id.btn_audio_playmode);
+        btnAudioPre = (Button) findViewById(R.id.btn_audio_pre);
+        btnAudioStartPause = (Button) findViewById(R.id.btn_audio_start_pause);
+        btnAudioNext = (Button) findViewById(R.id.btn_audio_next);
+        btnSwichLyric = (Button) findViewById(R.id.btn_swich_lyric);
+        ivIcon = (ImageView) findViewById(R.id.iv_icon);
+
+        ivIcon.setBackgroundResource(R.drawable.animation_list);
+        AnimationDrawable drawable = (AnimationDrawable) ivIcon.getBackground();
+        drawable.start();
+
+        btnAudioPlaymode.setOnClickListener(this);
+        btnAudioPre.setOnClickListener(this);
+        btnAudioStartPause.setOnClickListener(this);
+        btnAudioNext.setOnClickListener(this);
+        btnSwichLyric.setOnClickListener(this);
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2017-01-11 15:19:08 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if (v == btnAudioPlaymode) {
+            // Handle clicks for btnAudioPlaymode
+        } else if (v == btnAudioPre) {
+            // Handle clicks for btnAudioPre
+        } else if (v == btnAudioStartPause) {
+            // Handle clicks for btnAudioStartPause
+        } else if (v == btnAudioNext) {
+            // Handle clicks for btnAudioNext
+        } else if (v == btnSwichLyric) {
+            // Handle clicks for btnSwichLyric
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_system_audio_player);
+        findViews();
+
     }
 }
