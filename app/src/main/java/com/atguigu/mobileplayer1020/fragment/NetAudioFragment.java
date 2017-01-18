d4559d2125adc1d37ad622f152d7e082891ff9fe
atguiguafu <541433511@qq.com>
2017-01-18 09:19:36 +0800

    ButterKnife

--- app/src/main/java/com/atguigu/mobileplayer1020/fragment/NetAudioFragment.java
+++ app/src/main/java/com/atguigu/mobileplayer1020/fragment/NetAudioFragment.java
@@ -1,12 +1,16 @@
 package com.atguigu.mobileplayer1020.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.atguigu.mobileplayer1020.R;
 import com.atguigu.mobileplayer1020.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * 作者：尚硅谷-杨光福 on 2017/1/6 16:46
@@ -15,31 +19,42 @@ import com.atguigu.mobileplayer1020.base.BaseFragment;
  * 作用：网络音频
  */
 public class NetAudioFragment extends BaseFragment {
-    private TextView textView;
+
+    @InjectView(R.id.listview)
+    ListView listview;
+    @InjectView(R.id.progressbar)
+    ProgressBar progressbar;
+    @InjectView(R.id.tv_nomedia)
+    TextView tvNomedia;
+
     @Override
     public View initView() {
-        Log.e("TAG","网络音频ui初始化了。。");
-        textView = new TextView(mContext);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        return textView;
+        Log.e("TAG", "网络音频ui初始化了。。");
+        View view = View.inflate(mContext, R.layout.fragment_net_audio, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        Log.e("TAG","网络音频数据初始化了。。");
-        textView.setText("网络音频");
+        Log.e("TAG", "网络音频数据初始化了。。");
+        tvNomedia.setVisibility(View.VISIBLE);
+        tvNomedia.setText("hh");
     }
 
 
     @Override
     public void onRefrshData() {
         super.onRefrshData();
-        textView.setText("网络音频刷新");
 //        Log.e("TAG","onHiddenChanged。。"+this.toString());
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
 
 }
